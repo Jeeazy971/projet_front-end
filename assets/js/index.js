4d139ec79fbfb84525d25c35fb9fc9baab4d9557
Josué BAYIDIKILA <josue.dev@outlook.fr>
2022-10-12 18:48:35 +0200

    Finalisation  du front et création des sections

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,4 @@
+"use strict";
 /*
 Utilisez l'API Fetch pour collecter les données dans le backend.
 Les fichiers à récupérer sont :
@@ -11,33 +12,60 @@ vous trouverez leurs noms et descriptions dans les JSON téléchargés.
 
 */
 
-const header = document.querySelector('header');
-const monH1 = document.createElement('h1');
+const urlGuitar = "./back/guitars.json";
+const urlPopular = "./back/populars.json";
 
-fetch('./back/guitars.json')
-  .then(function (res) {
-    if (res.ok) {
-      return res.json();
+const contentGuitar = document.querySelector(".section-guitar > .content-guitar");
+const guitarAside = document.querySelector(".aside-guitar > .content-aside");
+
+
+const getAllGuitar = async () => {
+  const response = await fetch(urlGuitar);
+  const datas = await response.json();
+  return displayAllGuitar(datas);
+};
+
+// FETCH GUITARS
+
+const displayAllGuitar = (guitars) => {
+  guitars.forEach((electricGuitar) => {
+    try {
+      contentGuitar.innerHTML += `
+          <div class="electric-guitar">
+            <img src=./assets/images/guitares/${electricGuitar.imageUrl} alt=${electricGuitar.imageUrl}>
+            <p>${electricGuitar.name}</p>
+            <p>${electricGuitar.price} € ou <span class="price-bold">${electricGuitar.monthly} € / mois</span></p>
+          </div>
+          `;
+      contentGuitar.append(contentGuitar);
+    } catch (error) {
+      console.log(error.message);
     }
-  })
-  .then(datas => {
-    // Utilisation des données dé-jsonifiées dans l'objet data
-    datas.forEach((data) => {
-      console.log(data.name);
-      //header.innerHTML += `<h1>${data.name}</h1>`;
-    });
-
-
-
-    // console.table(datas);
-    // Regardez la structure du json, comprenez son contenu, intégrez-le
-    // dans votre HTML.
-  })
-  .catch(function(error) {
-    // Gestion des erreurs
-    console.error("Something goes wrong!");
-    console.error(error);
   });
+};
 
+// FETCH POPULARS GUITARS
 
-  
+const getAllPopluars = async () => {
+  const response = await fetch(urlPopular);
+  const datas = await response.json();
+  return displayAllPopular(datas);
+};
+
+const displayAllPopular = (popularGuitar) => {
+  popularGuitar.forEach((popular) => {
+    guitarAside.innerHTML += `
+      <div class="guitar-aside">
+      <img src=./assets/images/guitares/${popular.imageUrl} alt=${popular.imageUrl}>
+      <p>${popular.name}</p>
+      <p>${popular.price} € ou <span class="price-bold">${popular.monthly} € / mois</span></p>
+      </div>
+    `;
+    guitarAside.append(guitarAside);
+  });
+};
+
+getAllPopluars();
+
+getAllGuitar();
+displayAllGuitar();
